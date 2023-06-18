90c192c911dfeee07b1854665ff325d9221e788f
Maxime Bouillot <maxime.bouillot@withings.com>
2023-06-18 19:18:24 +0200

    Removed all full-buffer-based functions.

--- autoload/hex_edit_test.lua
+++ autoload/hex_edit_test.lua
@@ -1,58 +1,43 @@
 #!/usr/bin/env lua
 
-local hex_edit = require("hex_edit")
+local hex_streamer = require("hex_stream")
+local new_bin_streamer = require("bin_stream")
 
 local arr = ""
 for i=0,303 do
     arr = arr .. string.char(i % 256)
 end
-local buff = hex_edit.hd_buffer(arr)
-print(buff)
-arr_back = hex_edit.binarize_buffer(buff)
-if arr_back ~= arr then
-    print(arr_back)
-    print("---")
-    print(arr)
-end
 
--- compare the new stream implemenataion with the old one
-local hex_streamer = require("hex_stream")
-local streamer = hex_streamer(300)
-local new_buff = ""
+
+-- bin to hex
+local hex_streamer = hex_streamer(300)
+local buff = ""
 for i=1,#arr do
-    local new_lines = streamer:process(arr:sub(i,i))
+    local new_lines = hex_streamer:process(arr:sub(i,i))
     for i=1,#new_lines do
-        new_buff = new_buff .. new_lines[i] .. "\n"
+        buff = buff .. new_lines[i] .. "\n"
     end
 end
-local new_lines = streamer:finish()
+local new_lines = hex_streamer:finish()
 for i=1,#new_lines do
-    new_buff = new_buff .. new_lines[i] .. "\n"
+    buff = buff .. new_lines[i] .. "\n"
 end
-if new_buff ~= buff then
-    print(new_buff)
-    print("---")
-    print(buff)
-end
+print(buff)
 
-local new_bin_streamer = require("bin_stream")
+-- hex to bin
 local bin_streamer = new_bin_streamer()
-local err = bin_streamer:stream_dump(new_buff)
+local err = bin_streamer:stream_dump(buff)
 if err then
     print(err)
 end
 bin_streamer:finish()
-
-local new_arr_back = bin_streamer.lines[1]
+local arr_back = bin_streamer.lines[1]
 for i=2,#bin_streamer.lines do
-    new_arr_back = new_arr_back.."\n"..bin_streamer.lines[i]
+    arr_back = arr_back.."\n"..bin_streamer.lines[i]
 end
-if new_arr_back ~= arr_back then
-    print(#new_arr_back, #arr_back)
-    print("----")
-    print(new_arr_back)
-    print("<><><><>")
+if arr_back ~= arr then
     print(arr_back)
-    print("----")
+    print("---")
+    print(arr)
 end
 
--- autoload/vim_hex.lua
+++ autoload/vim_hex.lua
@@ -1,37 +1,6 @@
-local hex_edit = require("hex_edit")
-local module = {hex_edit = hex_edit}
+local module = {}
 local new_hex_streamer = require("hex_stream")
 local new_bin_streamer = require("bin_stream")
-
--- Sets the current buffer to the string s
-local set_buff = function(s)
-    local current_buffer = vim.buffer(false)
-    for i=0,#current_buffer do
-        current_buffer[1] = nil
-    end
-    local lines = hex_edit.split_line(s)
-    for i=1,#lines do
-        current_buffer:insert("")
-        current_buffer[i] = lines[i]
-    end
-    current_buffer[#lines+1] = nil
-end
-
--- Get the current buffer as a string
-local get_buff = function()
-    local current_buffer = vim.buffer(false)
-    local ret = ""
-    for i=1,#current_buffer do
-        ret = ret .. current_buffer[i] .. "\n"
-    end
-    if (vim.eval("b:vim_hex_trailing")) == 1 then
-        vim.command("let b:vim_hex_trailing = 0")
-        return ret
-    else
-        return ret:sub(1,#ret-1)
-    end
-end
-
 
 -- Transform the current buffer into an hexdump of itself
 module.hd_vim_buffer = function()
